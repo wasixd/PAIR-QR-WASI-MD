e29f737f93514327ed7d07b0a21828f17b9f90e3
the  develpor <164501586+wasixd@users.noreply.github.com>
2024-04-08 03:24:41 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -32,7 +32,7 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Chrome (Linux)", "", ""]
+                browser: ["wasiMd(Linux)", "", ""]
              });
              if(!Pair_Code_By_Wasi_Tech.authState.creds.registered) {
                 await delay(1500);
